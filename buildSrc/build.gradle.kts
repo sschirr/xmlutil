8de9c0d82e4512e1e3b6946ddd066e4032716f89
Paul de Vrieze <paul.devrieze@gmail.com>
2022-11-29 20:28:40 +0000

    Keep compilation work against the modern kotlin plugin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,7 +21,6 @@
 @file:Suppress("PropertyName")
 
 plugins {
-//    kotlin("jvm") version libs.versions.kotlin
     `kotlin-dsl`
 }
 
@@ -29,16 +28,11 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val kotlinVersion = libs.versions.kotlin.get()
-
 dependencies {
 //    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
-    compileOnly("org.jetbrains.kotlin:kotlin-native-utils:$embeddedKotlinVersion")
-    compileOnly("org.jetbrains.dokka:dokka-gradle-plugin:$embeddedKotlinVersion")
-    runtimeOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    runtimeOnly("org.jetbrains.kotlin:kotlin-native-utils:$kotlinVersion")
-    runtimeOnly("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    implementation("org.jetbrains.kotlin:kotlin-native-utils:${libs.versions.kotlin.get()}")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${libs.versions.dokka.get()}")
 }
 
 repositories {
